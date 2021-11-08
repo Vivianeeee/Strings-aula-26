5a656fc70a3ee820470017a92eaecec992640d4d
vivianeeee <vivianeoliveirandrade@gmail.com>
2021-11-08 20:13:20 -0300

    string aula 26

---
+++ js/main.js
@@ -0,0 +1,63 @@
+let string= 'instituto da oportunidade IOS';
+
+//ACESSANDO O PRIMEIRO CARACTER
+console.log(string.charAt(3));
+console.log(string[0]);
+
+console.clear();
+
+let texto= 'Viviane';
+console.log(texto.length);
+
+console.clear()
+
+let s= 'Hello Word'
+console.log(s.toUpperCase());
+
+let d= 'Hello Word'
+console.log(d.toLocaleLowerCase());
+
+console.clear()
+
+let str= 'Viviane';
+console.log(str.substring(0, 4));
+console.log(str.substring(4));
+
+console.clear()
+
+const str1 = 'A raposa é um animal esperto';
+
+const palavras = str1.split(' ');
+console.log(palavras[3]);
+
+const palavras = str1.split(' ');
+console.log(palavras[7]);
+
+console.clear()
+
+let mensagem = 'Venha para a Microsoft';
+console.log(mensagem);
+
+let novamensagem = mensagem.replace
+('Microsoft' , 'Ios');
+console.log(novamensagem);
+
+console.clear()
+
+let text = '     Olá alunas do IOS  ' ;
+console.log(text.trim());
+
+console.clear()
+
+let frase = ' Sou um aluno do IOS e o IOS é muito bom! ';
+
+//console.log(frase.indexOF('do'));
+//console.log(frase.indexOF('IOS'));
+
+//console.log(frase.lastIndexOF('IOS'));
+//console.log(frase.indexOF('IOS'));
+
+//console.log(frase.startsWith('D'));
+//console.log(frase.startsWith('Um'));
+
+//console.log(frase.endsWith('Bom!'));
